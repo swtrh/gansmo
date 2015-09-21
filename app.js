3c3df30acb3a8dca311ebe8533e99836dadc1cf3
Ove Petter Gansmo <opg@statoil.com>
2015-09-21 13:52:30 +0200

    Test

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var store = require('./routes/store');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/store', store);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
